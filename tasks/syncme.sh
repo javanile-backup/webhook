378358de199fa293ce36717515f8c4d99a0b9b84
francescobianco <info.francescobianco@gmail.com>
2017-10-06 15:01:39 +0200

    add sample scripts

--- tasks/syncme.sh
+++ tasks/syncme.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
+## Move to project directory
 cd $(dirname $0)/../
 
+## Get User and Group
+USER=${1:-$(id -u -n)}
+GROUP=${2:-$(id -u -n)}
+
+## Update from reposistory
 git checkout master
 git fetch --all
 git reset --hard origin/master
 
+## Update dependency
 composer install
 composer update
 
-chmod 777 logs/*.log
+## Apply permissions
+chown $USER:$GROUP * -R
+
+## Apply executables
+find . -name '*.sh' -type f | xargs chmod +x
