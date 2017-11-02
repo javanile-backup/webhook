0d235bc1b475b4503785920cb8a4512983635232
Francesco Bianco <info.francescobianco@gmail.com>
2017-11-02 23:38:33 +0100

    refactoring cron script

---
+++ src/Trigger.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Mount command for producer.
+ *
+ * PHP version 5
+ *
+ * @category ProducerCommand
+ *
+ * @author    Francesco Bianco <bianco@javanile.org>
+ * @copyright 2015-2017 Javanile.org
+ * @license   https://goo.gl/KPZ2qI  MIT License
+ */
+
+namespace Javanile\Webhook;
+
+class Trigger extends Manifest
+{
+    /**
+     * Get remote url for call webhook.
+     *
+     * @param mixed $url
+     */
+    public function getRemoteUrl($url = null)
+    {
+        if (!$url || $url == 'manifest') {
+            $manifest = $this->loadManifest();
+            $url = isset($manifest['url']) ? $manifest['url'] : 'http://localhost/';
+        }
+
+        return rtrim($url, '/') . '/webhook.php';
+    }
+
+    /**
+     * Get remote data for call webhook.
+     */
+    public function getRemoteData()
+    {
+        $data = [
+            'ref' => 'refs/heads/master',
+        ];
+
+        return json_encode($data);
+    }
+}
--- trigger.sh
+++ trigger.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-WEBHOOK=$(php webhook-tools.php remote-url $1)?hook=$2
-PAYLOAD=$(php webhook-tools.php remote-data $2)
-CONTENT=$(echo Content-Type: application/json)
-XSECRET=$(php webhook-tools.php secret $2)
+MANIFEST=manifest.json
+
+# echo Content-Type: application/json
+
+WEBHOOK=$(php trigger.php $MANIFEST remote-url $1)?hook=$2
+PAYLOAD=$(php trigger.php $MANIFEST remote-data $2)
+CONTENT=$(php trigger.php $MANIFEST content-type $2)
+XSECRET=$(php trigger.php $MANIFEST secret $2)
 XGITHUB=$(echo -n "${PAYLOAD}" \
     | openssl dgst -sha1 -hmac "%{XSECRET}" \
     | awk '{print "X-Hub-Signature: sha1="$2}')
@@ -14,7 +18,7 @@ curl \
     -X POST \
     -H "${CONTENT}" \
     -H "${XGITHUB}" \
-    --data "${PAYLOAD}" \
+    -d "${PAYLOAD}" \
     ${WEBHOOK}
 
 echo ""; echo "--> Done."
